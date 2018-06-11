c59eef2a27c9f6022b1de200033651510b83562d
Blake Kruppa <btkruppa513@gmail.com>
2018-06-11 19:22:30 -0400

    updated the build

--- week4/redux-demo/src/environment.ts
+++ week4/redux-demo/src/environment.ts
@@ -3,7 +3,7 @@ const dev = {
 }
 
 const prod = {
-  context: 'http://google.com/'
+  context: 'http://ec2-18-237-118-203.us-west-2.compute.amazonaws.com:3001/'
 }
 
 export const environment = process.env.NODE_ENV === 'production'
