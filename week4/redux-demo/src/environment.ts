81943544af37fa6ea210f0d1646cffeea439e097
Scott <sm.scottkmoore@gmail.com>
2018-06-12 14:47:26 -0400

    Created build

--- week4/redux-demo/src/environment.ts
+++ week4/redux-demo/src/environment.ts
@@ -3,7 +3,7 @@ const dev = {
 }
 
 const prod = {
-  context: 'http://ec2-18-237-118-203.us-west-2.compute.amazonaws.com:3001/'
+  context: 'http://ec2-54-164-83-42.compute-1.amazonaws.com:3001/'
 }
 
 export const environment = process.env.NODE_ENV === 'production'
